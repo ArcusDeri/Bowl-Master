369a9f6d644271b01f0ece6436c7eb92791b28cc
MARCIN\Marcin <michniak@yandex.com>
2017-11-03 16:51:24 +0100

    Created ball with mass of 7.1kg and a diameter of 21cm. Placed a texture on it and added a script which sets initial velocity and plays the sound of rolling.

---
+++ Assets/Ball.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	public float LaunchSpeed;
+
+	private Rigidbody MyRigidBody;
+	private AudioSource RollingSound;
+
+	// Use this for initialization
+	void Start () {
+		MyRigidBody = gameObject.GetComponent<Rigidbody> ();
+		RollingSound = gameObject.GetComponent<AudioSource> ();
+
+		Launch ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void Launch ()
+	{
+		MyRigidBody.velocity = new Vector3 (0, 0, LaunchSpeed);
+		RollingSound.Play ();
+	}
+}
