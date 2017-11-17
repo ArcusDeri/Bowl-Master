c3ab0f80fed01ad835311827d47ef3bc5a767a0f
MARCIN\Marcin <michniak@yandex.com>
2017-11-17 18:41:41 +0100

    Added Pin.cs script to prefabbed pins. Removed single pin prefab since nested prefabs are not connected with it, instead I have prefab of whole pin set. Pin.cs contains information about rotation treshold and it provides method indicating wether the pin is standing or not.

---
+++ Assets/Scripts/Pin.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour {
+	public float StandingThreshold;
+
+	void Start(){
+		StandingThreshold = transform.rotation.eulerAngles.y;
+	}
+
+	public bool IsStanding(){
+		Vector3 eulerRotation = transform.rotation.eulerAngles;
+		float tiltInX = Mathf.Abs(eulerRotation.x);
+		float tiltInZ = Mathf.Abs(eulerRotation.z);
+		
+		return tiltInX < StandingThreshold && tiltInZ < StandingThreshold;
+	}
+}
