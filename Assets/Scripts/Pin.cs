23bcb78e4bea105b22e65a57e3c29c5042357046
MARCIN\Marcin <michniak@yandex.com>
2017-11-19 12:13:28 +0100

    PinSetter is set to trigger type collider. When trigger is entered OnTriggerEnter() method checks if entered object is a ball. If so, the number of pins standing text color is set to red. When OnTriggerExit() checks if object is a pin and then destroy that pin. Simplified pin structure but it forced me to change pin rotation to 270 degrees thus IsStanding() needed small improvement to keep it working properly.

--- Assets/Scripts/Pin.cs
+++ Assets/Scripts/Pin.cs
@@ -6,12 +6,11 @@ public class Pin : MonoBehaviour {
 	public float StandingThreshold = 3f;
 
 	void Start(){
-		Debug.Log(StandingThreshold);
 	}
 
 	public bool IsStanding(){
 		Vector3 eulerRotation = transform.rotation.eulerAngles;
-		float tiltInX = Mathf.Abs(eulerRotation.x);
+		float tiltInX = Mathf.Abs(270 - eulerRotation.x);
 		float tiltInZ = Mathf.Abs(eulerRotation.z);
 		//it has to be like that cause eulerAngles are too precise surprisingly
 		if((tiltInX < StandingThreshold || tiltInX > 357) && (tiltInZ < StandingThreshold || tiltInZ > 357))
--- Assets/Scripts/PinSetter.cs
+++ Assets/Scripts/PinSetter.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class PinSetter : MonoBehaviour {
 
 	public Text StandingDisplay;
+
+	private bool BallInBox = false;
 	// Use this for initialization
 	void Start () {
 		
@@ -22,5 +24,19 @@ public class PinSetter : MonoBehaviour {
 			counter += pin.IsStanding() ? 1 : 0;
 		}
 		return counter;
+	}
+
+	void OnTriggerEnter(Collider collider){
+		var objectHit = collider.gameObject;
+		if(objectHit.GetComponent<Ball>()){
+			BallInBox = true;
+			StandingDisplay.color = Color.red;
+		}
+	}
+	void OnTriggerExit(Collider collider){
+		var objectLeft = collider.gameObject;
+		if(objectLeft.GetComponent<Pin>()){
+			Destroy(objectLeft);
+		}
 	}
 }
