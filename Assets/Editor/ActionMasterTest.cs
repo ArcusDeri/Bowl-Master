9d021ffb448f2a39685f87d48dee1174ddb73d60
MARCIN\Marcin <michniak@yandex.com>
2017-11-23 19:01:46 +0100

    Moved tests directory because of some issues with Unity finding them. Added tests for two cases: when bowling eight pins and when bowling three,then seven to cause "spare". After that added code in ActionMaster.cs to handle these tests.

---
+++ Assets/Editor/ActionMasterTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+[TestFixture]
+public class ActionMasterTest {
+
+	private ActionMaster MyActionMaster;
+	private ActionMaster.Action EndTurn = ActionMaster.Action.EndTurn;
+	private ActionMaster.Action Tidy = ActionMaster.Action.Tidy;
+	private ActionMaster.Action Reset = ActionMaster.Action.Reset;
+
+	[SetUp]
+	public void SetUp(){
+		MyActionMaster = new ActionMaster();
+	}
+
+	[Test]
+	public void T00PassingTest(){
+		Assert.AreEqual(1,1);
+	}
+
+	[Test]
+	public void T01_OneStrikeReturnsEndTurn(){
+		Assert.AreEqual(EndTurn, MyActionMaster.Bowl(10));
+	}
+
+	[Test]
+	public void T02_BowlEightReturnsTidy(){
+		Assert.AreEqual(Tidy,MyActionMaster.Bowl(8));
+	}
+
+	[Test]
+	public void T03_BowlSpareReturnsEndTurn(){
+		MyActionMaster.Bowl(3);
+		Assert.AreEqual(EndTurn,MyActionMaster.Bowl(7));
+	}
+
+}
--- Assets/Scripts/ActionMaster.cs
+++ Assets/Scripts/ActionMaster.cs
@@ -5,12 +5,23 @@ using UnityEngine;
 public class ActionMaster {
 
 	public enum Action {Tidy, Reset, EndTurn, EndGame}
+	
+	private int BowlNumber = 1;
 
 	public Action Bowl(int pins){
 		if(pins < 0 || pins > 10)
-			throw new UnityException("Incorrect number of pins to bowl.")
-		if(pins == 10)
+			throw new UnityException("Incorrect number of pins to bowl.");
+		if(pins == 10){
+			BowlNumber += 2;
 			return Action.EndTurn;
+		}
+		if(BowlNumber % 2 != 0){//Mid frame or last frame.
+			BowlNumber += 1;
+			return Action.Tidy;
+		}else if(BowlNumber % 2 == 0){//End of frame.
+			BowlNumber += 1;
+			return Action.EndTurn;
+		}
 
 		throw new UnityException("Not sure what action to return.");
 	}
