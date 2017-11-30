96a62c08340559c9565116f7a8cf2b12e1273c8b
MARCIN\Marcin <michniak@yandex.com>
2017-11-30 21:13:16 +0100

    Starting point for "Epic TDD challenge" from the course. Will attempt to uncomment tests one by one and make ScoreMaster.cs working. Commented out whole ActionMasterTest.cs to avoid running it for saving time in this challange.

--- Assets/Scripts/ScoreMaster.cs
+++ Assets/Scripts/ScoreMaster.cs
@@ -1,9 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class ScoreMaster {
 	
+	//returns list of cumulative scores like a nomral score card
+	public static List<int> ScoreCumulative(List<int> rolls){
+		var cumulativeScores = new List<int>();
+		int runningTotal = 0;
+		foreach (int frameScore in ScoreFrames(rolls))
+		{
+			runningTotal += frameScore;
+			cumulativeScores.Add(runningTotal);
+		}
+
+		return cumulativeScores;
+	}
+
+	//returns list of individual frame scores, not cumulative
 	public static List<int> ScoreFrames(List<int> rolls){
 		List<int> frameList = new List<int>();
 
