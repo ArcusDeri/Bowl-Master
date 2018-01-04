2bebc47715920c1d949d00c0cca60a4357a2effb
MARCIN\Marcin <michniak@yandex.com>
2018-01-04 12:53:01 +0100

    Changed UI composition a bit. Added scores display and added .cs class to it. Empty every frame text on initialization.

---
+++ Assets/Scripts/Scores.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Scores : MonoBehaviour {
+
+	public Text [] RollTexts, FrameTexts;
+
+	// Use this for initialization
+	void Start () {
+		foreach(var textDisplay in RollTexts)
+			textDisplay.text = "";
+		foreach(var textDisplay in FrameTexts)
+			textDisplay.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
